44e490aa3ec4e9d620995038e2dde8cb66067320
Marek Skotny <marekskotny@gmail.com>
2024-09-09 21:53:41 +0200

    1 - Connection with mongoDB Atlas

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DepositModule } from './module/deposit/deposit.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    DepositModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
